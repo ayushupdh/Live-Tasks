f5cdaedc532a2e105ee7cbf2d44c35cd0a628523
ayushupdh <aayushupadhyay14@gmail.com>
2020-08-02 18:56:01 -0500

    Final fix for routing issues

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use(notesRouter);
 app.use(userRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
 
